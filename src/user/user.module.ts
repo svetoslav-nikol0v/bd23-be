922c870371485bb8d0c29d6f3c2cbc75726f6188
Svet <svetoslav.nikolov@limechain.tech>
2025-02-11 21:40:07 +0200

    import user module in auth module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,13 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
-@Global()
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
     controllers: [UserController],
     providers: [UserService],
-    exports: [UserService, TypeOrmModule],
+    exports: [UserService],
 })
 export class UserModule {}
